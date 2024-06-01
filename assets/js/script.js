5f0e91267ebdf6f91deee2dc0365d2660d103cb9
Marco Navarrete <marconav2000@gmail.com>
2024-05-31 20:00:46 -0400

    Creación y modificacion de archivo Script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,43 @@
-precio = 400000
+const precio = 400000
+let total = 0
+let cantidad = 0
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioProducto = document.querySelector("#precio-producto");
+precioProducto.innerHTML = precio
+
+cantidadProducto = document.querySelector("#cantidad")
+cantidadProducto.innerHTML = cantidad
+
+totalValor = document.querySelector("#valor-total")
+
+btnSumar = document.querySelector("#sumar")
+btnRestar = document.querySelector("#restar")
+
+
+
+btnSumar.addEventListener('click', () => {
+    cantidad += 1
+    cantidadProducto.innerHTML = cantidad
+    total = precio * cantidad
+    totalValor.innerHTML = total
+}
+)
+
+btnRestar.addEventListener('click', () => {
+    
+
+
+if (cantidad < 1) {
+    cantidad = 0
+} 
+else {
+    cantidad -= 1
+}
+
+
+
+cantidadProducto.innerHTML = cantidad
+total = precio * cantidad
+totalValor.innerHTML = total
+}
+)
